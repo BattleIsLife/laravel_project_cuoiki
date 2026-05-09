a136c9d691faaaa889920110d87fff4a45a6aff5
BattleIsLife <nothinginterested123@gmail.com>
2026-05-09 04:50:25 +0000

    Tạo file view test mẫu

---
+++ resources/views/test.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="{{ @asset('css/bootstrap-5.3.8/bootstrap.min.css') }}">
+    <title>Document</title>
+</head>
+<body>
+    <h1 class="text-center">Thử nghiệm với blade</h1>
+</body>
+
+<script src="{{ @asset('js/bootstrap-5.3.8/bootstrap.min.js') }}"></script>
+</html>
