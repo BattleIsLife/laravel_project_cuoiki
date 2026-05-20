fe60a26b4e482e0b445c281b72b2789ba7ae3ea3
BattleIsLife <nothinginterested123@gmail.com>
2026-05-20 04:44:54 +0000

    Thêm thuộc tính unique cho bảng fictions

--- database/migrations/2026_05_14_212450_create_fictions_table.php
+++ database/migrations/2026_05_14_212450_create_fictions_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
     {
         Schema::create('fictions', function (Blueprint $table) {
             $table->uuid('id')->primary();
-            $table->string('fiction_name', 100)->nullable(false);
+            $table->string('fiction_name', 100)->unique()->nullable(false);
             $table->foreignUuid('user_id')->references('id')->on('users')->onDelete('cascade');
             $table->foreignUuid('series_id')->nullable(true)->references('id')->on('series')->onDelete('set null');
             $table->text('description')->nullable(true);
