7bcdcbedce3792183350f1e03b7bf39141ef600e
BattleIsLife <nothinginterested123@gmail.com>
2026-05-22 09:42:58 +0000

    Thêm giao diện thông tin tk

---
+++ resources/views/user/profile/account_info.blade.php
@@ -0,0 +1,28 @@
+@extends('user.profile')
+@section('user_profile_component')
+<div class="text-center">
+    <h2>Thông tin tài khoản</h2>
+    @if (session()->has('error'))
+            <div class="alert alert-danger">
+                {{ session()->get('error') }}
+            </div>
+    @endif
+
+    @if (session()->has('success'))
+        <div class="alert alert-success">
+            {{ session()->get('success') }}
+        </div>
+    @endif
+</div>
+<div class="row">
+    <div class="col-sm-6">
+        <p>Tên tài khoản: <i><span id="username"></span></i></p>
+    </div>
+
+    <div class="col-sm-6">
+        <p>Email: <i><span id="email"></span></i></p>
+    </div>
+    <p><a href=">">Đổi mật khẩu</a></p>
+</div>
+
+@endsection
