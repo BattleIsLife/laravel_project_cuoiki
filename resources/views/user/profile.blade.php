2c5070e176a8fdab81a1fa526aa0a0e5985d6875
BattleIsLife <nothinginterested123@gmail.com>
2026-05-22 09:45:00 +0000

    Thêm giao diện profile người dùng tổng

--- resources/views/user/profile.blade.php
+++ resources/views/user/profile.blade.php
@@ -1,3 +1,30 @@
-<div>
-    <!-- The only way to do great work is to love what you do. - Steve Jobs -->
+@extends('main')
+@section('content')
+<div class="container-fluid p-3">
+    <div class="row">
+        <div class="col-sm-3">
+            <ul class="nav nav-pills flex-column">
+                <!-- Tab chuyển đổi -->
+                <li class="nav-item">
+                    <a class="nav-link" href="">Thông tin tài khoản</a>
+                </li>
+
+                <li class="nav-item">
+                    <a class="nav-link" href="">Danh sách truyện</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="">Danh sách series</a>
+                </li>
+
+                <li class="nav-item mt-3">
+                    <a class="nav-link bg-secondary text-white" href="">Xóa tài khoản</a>
+                </li>
+            </ul>
+            <hr class="d-sm-none">
+        </div>
+        <div class="col-sm-9">
+            @yield('user_profile_component')
+        </div>
+    </div>
 </div>
+@endsection
