d62aa747da176a15e4229d406d724f8bf2d6f5cb
BattleIsLife <nothinginterested123@gmail.com>
2026-05-25 11:33:22 +0000

    Thêm mới model LikeFictionHistory

---
+++ app/Models/LikeFictionHistory.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class LikeFictionHistory extends Model
+{
+    protected $table="like_fiction_history";
+
+    protected $fillable = [
+        'user_id',
+        'comment_id'
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class,'user_id');
+    }
+
+    public function fiction()
+    {
+        require $this->belongsTo(Fiction::class, 'fiction_id');
+    }
+}
