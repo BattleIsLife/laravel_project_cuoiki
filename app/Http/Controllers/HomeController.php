f90d1e7c99c17dfece68381ce1a7c439be73d97e
BattleIsLife <nothinginterested123@gmail.com>
2026-05-25 02:50:56 +0000

    Áp dụng HomeController của Thế Đạt + Thêm mới 1 vài view

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -5,12 +5,48 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 
 use App\Models\User;
+use App\Models\Fiction;
+use App\Models\Series;
+use Illuminate\Support\Facades\Cache;
 
 class HomeController extends Controller
 {
     public function index()
     {
-        return view('user.home');
+        // Top 8 truyện hot nhất theo lượt xem
+        $hotFictions = Fiction::whereHas('chapters')->withSum('chapters', 'watch_count') // Tự động tính tổng cột 'views' trong bảng 'chapters'
+                                ->orderBy('chapters_sum_watch_count', 'desc') // Sắp xếp theo thực thể vừa tính
+                                ->take(8)
+                                ->get();
+
+
+        // Top 8 series hot nhất theo lượt xem
+        $hotSeries = Series::whereHas('fictions')->withSum('chapters', 'watch_count') // Tự động tính tổng cột 'views' trong bảng 'chapters'
+                            ->orderBy('chapters_sum_watch_count', 'desc') // Sắp xếp theo thực thể vừa tính
+                            ->take(8)
+                            ->get();
+
+        // Top 5 bài viết mới nhất của moderator
+        // $moderatorPosts = \App\Models\ModeratorPost::with('moderator')
+        //     ->latest()
+        //     ->limit(5)
+        //     ->get();
+        $data = [
+            'hotFictions' => $hotFictions,
+            'hotSeries'   => $hotSeries
+        ];
+
+        return view('user.home', $data);
+    }
+
+    public function all_fictions()
+    {
+        return view('fiction.all_fictions');
+    }
+
+    public function all_series()
+    {
+        return view('series.all_series');
     }
 
     // public function test()
