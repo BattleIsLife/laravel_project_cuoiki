f756c78ce1c1f9377a6253e4dafe9803003b9d63
BattleIsLife <nothinginterested123@gmail.com>
2026-05-26 02:53:37 +0000

    Đơn giản hóa giao diện sửa series, chuyển thành 2 tab: Sửa thông tin series và sửa danh sách truyện trong series

--- resources/views/fiction/edit_fictions.blade.php
+++ resources/views/fiction/edit_fictions.blade.php
@@ -2,50 +2,23 @@
 @section('content')
 
 <div class="container-fluid">
-    <ul class="nav nav-tabs justify-content-center" id="fictionEditTabs" role="tablist">
-        <li class="nav-item" role="presentation">
-            <button class="nav-link" 
-                    id="info-tab" 
-                    data-bs-toggle="tab" 
-                    data-bs-target="#edit_fiction_info" 
-                    type="button" 
-                    role="tab" 
-                    aria-controls="edit_fiction_info" 
-                    aria-selected="false">
-                Thông tin truyện
-            </button>
-        </li>
-        <li class="nav-item" role="presentation">
-            <button class="nav-link active" 
-                    id="chapters-tab" 
-                    data-bs-toggle="tab" 
-                    data-bs-target="#edit_chapter_list" 
-                    type="button" 
-                    role="tab" 
-                    aria-controls="edit_chapter_list" 
-                    aria-selected="true">
-                Mục lục chương
-            </button>
-        </li>
-    </ul>
+    <ul class="nav nav-tabs justify-content-center">
+    <li class="nav-item">
+        <a class="nav-link active" data-bs-toggle="tab" href="#edit_fiction_info">Thông tin truyện</a>
+    </li>
+    <li class="nav-item">
+        <a class="nav-link" data-bs-toggle="tab" href="#edit_chapter_list">Mục lục chương</a>
+    </li>
+</ul>
 
-    <div class="tab-content id="fictionEditTabsContent">
-        
-        <div class="tab-pane fade" 
-             id="edit_fiction_info" 
-             role="tabpanel" 
-             aria-labelledby="info-tab">
-            @include('fiction.partial.edit_fiction_info')
-        </div>
-
-        <div class="tab-pane fade show active" 
-             id="edit_chapter_list" 
-             role="tabpanel" 
-             aria-labelledby="chapters-tab">
-            @include('fiction.partial.chapter_list')
-        </div>
-        
+<div class="tab-content">
+    <div class="tab-pane container active" id="edit_fiction_info">
+        @include('fiction.partial.edit_fiction_info')
     </div>
+    <div class="tab-pane container fade" id="edit_chapter_list">
+        @include('fiction.partial.chapter_list')
+    </div>
+</div>
 </div>
 <script src="{{ @asset('js/edit_fiction.js') }}"></script>
 @endsection
