93d2a1d90a6d0daf292ce464d6560bf617ac6692
BattleIsLife <nothinginterested123@gmail.com>
2026-05-26 08:48:57 +0000

    Thêm model comment bài đăng

---
+++ app/Models/ModeratorPostComment.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
+
+class ModeratorPostComment extends Model
+{
+    //
+    use HasUuids;
+
+    protected $table = 'moderator_post_comments';
+
+    protected $fillable = [
+        'post_id',
+        'user_id',
+        'content',
+        'parent_comment'
+    ];
+
+    public function post()
+    {
+        $this->belongsTo(ModeratorPost::class, 'post_id');
+    }
+
+    public function user()
+    {
+        $this->belongsTo(User::class, 'user_id');
+    }
+
+    public function upvote_history()
+    {
+        $this->hasMany(UpvoteModeratorPostCommentHistory::class, 'comment_id');
+    }
+}
