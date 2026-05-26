5456fa34f42ab9356b0598616419a94d44ea5507
BattleIsLife <nothinginterested123@gmail.com>
2026-05-26 02:50:55 +0000

    Cập nhật route thêm, sửa, xóa series

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\Author\AuthorSeriesController;
 use Illuminate\Support\Facades\Route;
 
 Route::controller(HomeController::class)->group(function(){
@@ -48,8 +49,24 @@ Route::prefix('author')->middleware('check.user.login')->group(function(){
 
     // Series
     // Danh sách series
-    Route::get('/series_list', [ProfileController::class, 'series_list'])->name('user.series_list');
+    Route::get('/series_list', [AuthorSeriesController::class, 'index'])->name('user.series_list');
 
+    // Tạo series
+    Route::get('/new_series', [AuthorSeriesController::class, 'create'])->name('user.new_series');
+    Route::post('/new_series', [AuthorSeriesController::class, 'create_attempt']);
+
+    // Sửa series
+    Route::get('/edit_series/{id}', [AuthorSeriesController::class, 'edit']);
+    Route::put('/edit_series/{id}', [AuthorSeriesController::class, 'edit_attempt']);
+
+    // Thêm truyện vào series
+    Route::post('/edit_series/{id}/add_fiction', [AuthorSeriesController::class, 'add_fiction_to_series']);
+
+    // Xóa truyện khỏi series
+    Route::delete('/edit_series/remove_fiction/{id}', [AuthorSeriesController::class, 'delete_fiction_from_series']);
+
+    // Xóa series
+    Route::delete('/delete_series/{id}', [AuthorSeriesController::class, 'delete']);
 
     // // Đăng xuất tài khoản
     Route::post('/logout', [AuthController::class, 'logout']);
