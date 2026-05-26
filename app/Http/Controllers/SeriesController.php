caa9d81d5407aa67509ca4604480bc70a69b3ebc
BattleIsLife <nothinginterested123@gmail.com>
2026-05-26 02:47:24 +0000

    Cho SeriesController chỉ hiển thị những series đã có fiction

--- app/Http/Controllers/SeriesController.php
+++ app/Http/Controllers/SeriesController.php
@@ -10,50 +10,22 @@ class SeriesController extends Controller
 {
     public function index()
     {
-        $allSeries = Series::with('author')
+        $allSeries = Series::with('fictions')
             ->latest()
-            ->paginate(20);
+            ->paginate(10);
 
         return view('series.all_series', compact('allSeries'));
     }
 
-    public function show($seriesId)
+    public function show(string $seriesId)
     {
-        $series = Series::with(['author', 'fictions'])
+        $series = Series::with('fictions')
             ->findOrFail($seriesId);
 
         $fictions = $series->fictions()
-            ->latest()
-            ->paginate(20);
+                    ->latest()
+                    ->paginate(10);
 
         return view('series.detail_series', compact('series', 'fictions'));
-    }
-
-    public function create()
-    {
-        return view('series.new_series');
-    }
-
-    public function edit($seriesId)
-    {
-        $series = Series::where('id', $seriesId)
-            ->where('user_id', Auth::id())
-            ->firstOrFail();
-
-        return view('series.edit_series', compact('series'));
-    }
-
-    public function delete($seriesId)
-    {
-        $series = Series::where('id', $seriesId)
-            ->where('user_id', Auth::id())
-            ->firstOrFail();
-
-        $series->delete();
-
-        return response()->json([
-            'success' => true,
-            'message' => 'Đã xóa series.',
-        ]);
     }
 }
