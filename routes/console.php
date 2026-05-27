56fa15fc30e0a576c5bead0bd1feb906e81d3ee4
BattleIsLife <nothinginterested123@gmail.com>
2026-05-27 02:40:13 +0000

    Thêm cơ chế tự động kiểm tra và unblock người dùng đã bị chặn (mỗi giờ kiểm tra 1 lần)

--- routes/console.php
+++ routes/console.php
@@ -2,7 +2,17 @@
 
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Schedule;
+use App\Models\User;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
+
+
+// Tự động kiểm tra và unblock người dùng đã bị chặn
+Schedule::call(function () {
+    User::where('blocked_until', '<=', now())
+        ->update(['blocked_until' => null]);
+})->hourly();
+
