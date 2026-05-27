b39e3ef837d132a66047d92045eae50d5f99baea
BattleIsLife <nothinginterested123@gmail.com>
2026-05-27 01:48:46 +0000

    Thu nhỏ độ rộng của container đọc truyện

--- resources/views/chapter/detail_chapters.blade.php
+++ resources/views/chapter/detail_chapters.blade.php
@@ -2,7 +2,7 @@
 @section('content')
 <div class="container-sm mt-3 p-4">
     <h3 class="text-center">{{ $chapter->chapter_name }}</h3>
-    <div class="container mt-2 p-3 card">
+    <div class="container mt-2 p-3 card" style="max-width: 1100px">
         {!! $chapter->content !!}
     </div>
 </div>
