a61d21fd03c0349c7d4091c6ff41f77b2806cd1e
BattleIsLife <nothinginterested123@gmail.com>
2026-06-02 14:28:08 +0000

    Thêm modal thay đổi vai trò cho moderator và cải thiện chức năng tìm kiếm

--- resources/views/admin/profile/all_moderators.blade.php
+++ resources/views/admin/profile/all_moderators.blade.php
@@ -14,6 +14,10 @@
         </div>
     @endif
 </div>
+<form class="d-flex" role="search" method="get" action="{{ route('admin.moderator_list') }}">
+    <input class="form-control me-2" type="search" name="q" value="{{ $keyword ?? '' }}" placeholder="Tìm kiếm tên quản trị viên, email" aria-label="Search">
+    <button class="btn btn-success" type="submit">Search</button>
+</form>
 <div class="table-responsive-md">
     <table class="table table-striped">
         <thead>
@@ -59,7 +63,18 @@
                         }
                     @endphp
                     <td>{{ $permission_name }}</td>
-                    <td>Nút bấm sẽ ở đây!!</td>
+                    <td><button type="button" 
+                                class="btn btn-danger" 
+                                data-bs-toggle="modal" 
+                                data-bs-target="#changeInfoModal"
+                                data-id="{{ $moderator->id }}"
+                                data-username="{{ $moderator->username }}"
+                                data-email="{{ $moderator->email }}"
+                                data-permission="{{ $moderator->permission }}"
+                                onclick="get_user_info(this)"
+                                @if ($moderator->permission === 'admin') disabled @endif>
+                            Hành động
+                        </button></td>
                 </tr>
             @empty
                 <tr><td colspan="5" class="text-center">Chưa có quản trị viên nào</td></tr>
@@ -68,7 +83,59 @@
     </table>
 </div>
 
+<!-- The Modal -->
+<div class="modal" id="changeInfoModal">
+  <div class="modal-dialog modal-dialog-centered">
+    <div class="modal-content">
+        <form action="" method="post" id="changeUserInfoForm">
+            @csrf
+            @method('put')
+            <!-- Modal Header -->
+            <div class="modal-header">
+            <h4 class="modal-title">Thay đổi vai trò của moderator</h4>
+            <button type="button" class="btn-close" data-bs-dismiss="modal"></button>
+            </div>
+
+            <!-- Modal body -->
+            <div class="modal-body">
+                <div class="mb-3 mt-3">
+                    <label for="username" class="form-label">Tên người dùng:</label>
+                    <input type="text" class="form-control" disabled
+                            id="username" name="username" readonly value="Tên người dùng ở đây!!">
+                </div>
+
+                <div class="mb-3 mt-3">
+                    <label for="email" class="form-label">Email:</label>
+                    <input type="email" class="form-control" id="email" disabled readonly value="Email ở đây">
+                    <div class="invalid-feedback" id="emailError"></div>
+                </div>
+
+                <div class="mb-3 mt-3">
+                    <label for="blocked_until" class="form-label">Vai trò</label>
+                    <select class="form-select" id="permission" name="permission">
+                        <option value="none" selected>Không có</option>
+                        <option value="user_moderator">Quản trị người dùng</option>
+                        <option value="post_moderator">Quản trị bài đăng</option>
+                    </select>
+                </div>
+            </div>
+
+            <!-- Modal footer -->
+            <div class="modal-footer">
+                <button type="button" class="btn btn-danger" data-bs-dismiss="modal" onclick="close_btn(this)">Close</button>
+                <button type="submit" class="btn btn-success">Thay đổi</button>
+            </div>
+        </form>
+    </div>
+  </div>
+</div>
+
 <div class="d-flex justify-content-center mt-4">
     {{ $moderators->links('pagination::bootstrap-5') }}
 </div>
+
+<script>
+    const BASE_URL = "{{ url('/') }}";
+</script>
+<script src="{{ @asset('js/moderator/moderator_info.js') }}"></script>
 @endsection
