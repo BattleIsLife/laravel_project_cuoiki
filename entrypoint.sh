fada6d9dfcea63317545873170da27a6714f26dc
BattleIsLife <nothinginterested123@gmail.com>
2026-06-03 11:25:04 +0000

    Thêm entrypoint

---
+++ entrypoint.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+set -e
+# Kiểm tra đã tồn tại file chưa, nếu chưa thì bắt đầu tiến hành cài đặt
+if [ -z "$(ls -A /var/www/html/vendor)" ]; then
+    echo "Tiến hành cài đặt dependencies"
+    composer install --no-dev --optimize-autoloader
+
+    # Tạo app key
+    echo "Generating app key..."
+    php artisan key:generate
+
+    # Migration
+    echo "Tiến hành migration"
+    php artisan migrate --force
+    php artisan db:seed --force
+fi
+
+chown -R www-data:www-data /var/www/html/storage
+
+exec "$@"
